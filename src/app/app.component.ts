df1b01343f490fe97ce00355f6bdf56890445dc1
HenryDewa <henry@finasmart.com>
2023-08-14 10:16:58 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,16 @@ import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 export class AppComponent {
   src = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
   totalPages: number = 0;
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.totalPages);
+  }
 
   /**
    * Page change callback, which is called when a page is changed (called multiple times)
    *
    * @param e number
    */
-   pageChange(e: number) {
+  pageChange(e: number) {
     console.log('(page-change)', e + '/' + this.totalPages);
   }
 
@@ -24,7 +26,8 @@ export class AppComponent {
    * Get pdf information after it's loaded
    * @param pdf pdf document proxy
    */
-   afterLoadComplete(pdf: PDFDocumentProxy) {
+  afterLoadComplete(pdf: PDFDocumentProxy) {
     this.totalPages = pdf.numPages;
+    console.log(this.totalPages);
   }
 }
